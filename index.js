5987bd83bd0f636896adf06067f6bf11b12f5615
Crosbydev <xxristuaji141@gmail.com>
2023-07-31 16:24:34 +0700

    fix untuk documentasi

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const mongoString = process.env.DATABASE_URL;
 const swaggerUi = require('swagger-ui-express');
-const swaggerSumDocument = require ('./swaggerUI.json');
+const swaggerDefinition = require('./swaggerdocs.json'); 
 
 mongoose.connect(mongoString, {useNewUrlParser: true, useUnifiedTopology: true});
 const database = mongoose.connection;
@@ -18,18 +18,20 @@ database.once('connected', () => {
     console.log('Database Connected');
 })
 const app = express();
-const showAllowedMethods = (req, res, next) => {
-    res.setHeader('Allow', 'GET');
-    next();
-  };
+const swaggerDocs = swaggerDefinition;
 
-// app.get('/', (req, res) => {
-//     res.send('Ini adalah halaman utama dengan daftar metode yang diizinkan.');
-// });
 
 app.use(express.json());
 app.use('/api/heroes', pahlawan)
 app.use('/api/auth', routes);
-app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerSumDocument));
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
 
 app.listen(process.env.PORT || 3002, console.log(`Server Started at ${process.env.PORT}!`))
+
+
